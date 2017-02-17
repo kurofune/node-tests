838949b1ae503230f4a1dcc3574a4bcbe820a928
Nate Gross <nate@outcastgames.com>
2017-02-17 08:13:40 -0800

    added more tests

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.status(404).send({
+        error: 'Page not found',
+        name: 'Todo App v1.0'
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.send([{
+        name: 'Nate',
+        age: 41
+    }, {
+        name: 'Joe',
+        age: 45
+    }, {
+        name: 'Fred',
+        age: 53
+    }]);
+});
+
+app.listen(3000);
+
+module.exports.app = app;
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,37 +1,46 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-    var res = utils.add(33, 11);
+describe('Utils', () => {
 
-    expect(res).toBe(44).toBeA('number');
-    
-    // if (res !== 44) {
-    //     throw new Error(`Expected 44, but got ${res}`);
-    // }
-});
+    describe('#add', () => {
 
-it('should async add two numbers', (done) => {
-    utils.asyncAdd(4, 3, (sum) => {
-        expect(sum).toBe(7).toBeA('number');
-        done();
+        it('should add two numbers', () => {
+            var res = utils.add(33, 11);
+
+            expect(res).toBe(44).toBeA('number');
+            
+            // if (res !== 44) {
+            //     throw new Error(`Expected 44, but got ${res}`);
+            // }
+        });
     });
-});
 
-it('should square a number', () => {
-    var res = utils.square(10);
 
-    expect(res).toBe(100).toBeA('number');
-    
-    // if (res !== 100) {
-    //     throw new Error(`Expected 100, but got ${res}`);
-    // }
-});
 
-it('should asyc square a number', (done) => {
-    utils.asyncSquare(3, (square) => {
-        expect(square).toBe(9).toBeA('number');
-        done();
+
+    it('should async add two numbers', (done) => {
+        utils.asyncAdd(4, 3, (sum) => {
+            expect(sum).toBe(7).toBeA('number');
+            done();
+        });
+    });
+
+    it('should square a number', () => {
+        var res = utils.square(10);
+
+        expect(res).toBe(100).toBeA('number');
+        
+        // if (res !== 100) {
+        //     throw new Error(`Expected 100, but got ${res}`);
+        // }
+    });
+
+    it('should asyc square a number', (done) => {
+        utils.asyncSquare(3, (square) => {
+            expect(square).toBe(9).toBeA('number');
+            done();
+        });
     });
 });
 
